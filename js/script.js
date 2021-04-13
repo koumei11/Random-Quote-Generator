3b4d77d2d348a438ce38cf894ffaeb8a316f9d23
KOMEI KINJO <miketyson40166@gmail.com>
2021-04-13 12:22:00 +0900

    Create quote array with objects.

--- js/script.js
+++ js/script.js
@@ -10,13 +10,48 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+const quote = [
+  {
+    quote: 'With great power comes great responsibility.',
+    source: 'Peter Benjamin Parker',
+    citation: 'Spider-Man',
+    year: 2002
+  },
+  {
+    quote: 'It was Beauty killed the Beast.',
+    source: 'Carl Denham',
+    citation: 'King Kong',
+    year: 2005
+  },
+  {
+    quote: 'After all, tomorrow is another day!',
+    source: 'Katie Scarlett O\'Hara',
+    citation: 'Gone With the Wind',
+    year: 1952
+  },
+  {
+    quote: 'Roads? Where we\'re going we don\'t need roads.',
+    source: 'Doc',
+    citation: 'Back to the Future',
+    year: 1985
+  },
+  {
+    quote: `Hey. Don't ever let somebody tell you... 
+            You can't do something. Not even me. 
+            You got a dream... You gotta protect it. 
+            People can't do somethin' themselves, 
+            they wanna tell you you can't do it. 
+            If you want somethin', go get it. 
+            Period.`,
+    source: 'Christopher "Chris" Paul Gardner',
+    citation: 'The Pursuit of Happyness',
+    year: 2007
+  }
+];
 
 /***
  * `getRandomQuote` function
 ***/
-
 
 
 /***
